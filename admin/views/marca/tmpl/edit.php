857951805c64364567a051f30185d8f929ed8c30
solucion40 <ricardolau6@gmail.com>
2016-04-30 12:34:29 +0200

    Error encontrado porque no guardaba registro Marca- Olalla

--- admin/views/marca/tmpl/edit.php
+++ admin/views/marca/tmpl/edit.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die('Restricted access');
 JHtml::_('behavior.tooltip');
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_coche&layout=edit&id='.(int) $this->item->id); ?>"
-      method="post" name="adminForm" id="pruebasimon-form">
+      method="post" name="adminForm" id="adminForm">
         <fieldset class="adminform">
                 <legend><?php echo JText::_( 'Detalles de la marca' ); ?></legend>
                 <ul class="adminformlist">
