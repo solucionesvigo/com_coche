c72ba3e680567ade2eb3d49ec804f1cc66f50885
solucion40 <ricardolau6@gmail.com>
2016-05-05 12:24:03 +0200

    Elimino funcion constructor y task, ya que no hace falta. Añado requerido a alguno campos en los formularios.

--- admin/models/marcas.php
+++ admin/models/marcas.php
@@ -9,26 +9,7 @@ defined('_JEXEC') or die;
 class CocheModelMarcas extends JModelList
 {
         
-        public function __construct($config = array())
-		{
-
-			if ($_REQUEST['task'] == 'add'):
-				echo ' Debo enviarlos a funcion añadir, edit, .... ';
-				$this->task();
-			endif;
-			 
-			
-			
-			echo '<pre>';
-			echo ' Entro model/marcas.php en funcion construct()<br/>';
-			echo ' Debo comprobar de donde vengo para saber si tengo que ir marcar o no... ';
-			echo '</pre>';
-               
-			parent::__construct($config);
-
-		}
-        
-        
+       
         /**
          * M�todo para crear una consulta SQL para cargar los datos de la lista.
          *
@@ -47,10 +28,6 @@ class CocheModelMarcas extends JModelList
                 $query->select('id,nombre,imagen');
                 $query->from('#__coche_marcas');
                 return $query;
-        }
-        protected function task()
-		{
-				echo ' grabando';
         }
         
         
